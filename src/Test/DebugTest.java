b3edad7e43ee18f057be885f7016aa6cc9ce21d3
blosseden <krst7336@naver.com>
2020-05-01 16:13:07 +0900

    안농 ㅇ.ㅇ!!

---
+++ src/Test/DebugTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class DebugTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("빼앵1");
+		System.out.println("빼앵2");
+		System.out.println("빼앵3");
+		System.out.println("빼앵4");
+		System.out.println("빼앵5");
+
+	}
+
+}
